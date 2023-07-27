da699c2cfd1179c588b06f02e5761e4a8be6bf83
TJ Hayward <tjhayward@tjs-macbook-pro.home>
2023-07-27 14:55:51 +0100

    stack size is zero when pushed and popped

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,48 +1,34 @@
-const { beforeEach } = require("node:test");
-
 const stackFactory = () => {
-  let empty = true;
   let count = 0;
   return {
-    isEmpty: () => empty,
+    isEmpty: () => count === 0,
     size: () => count,
     push: () => {
-      empty = false;
       count += 1;
     },
     pop: () => {
-      empty = true;
+      count -= 1;
     },
   };
 };
 
-let stack = stackFactory();
+let stack;
 
-describe("the stack canary spec", () => {
+describe("a stack", () => {
   beforeEach(() => {
     stack = stackFactory();
   });
-  it("shows the infrastructure works", () => {
-    expect(true).toBe(true);
-  });
-});
-
-describe("a stack", () => {
   it("starts empty", () => {
-    stack = stackFactory();
     expect(stack.isEmpty()).toBe(true);
   });
   it("starts with stack size of 0", () => {
-    stack = stackFactory();
     expect(stack.size()).toBe(0);
   });
   it("is not empty when pushed", () => {
-    stack = stackFactory();
     stack.push();
     expect(stack.isEmpty()).toBe(false);
   });
   it("stack size is 1 when pushed", () => {
-    stack = stackFactory();
     stack.push();
     expect(stack.size()).toBe(1);
   });
@@ -51,8 +37,11 @@ describe("a stack", () => {
     stack.pop();
     expect(stack.isEmpty()).toBe(true);
   });
-
-  it.todo("stack size is 0 when pushed and popped");
+  it("stack size is 0 when pushed and popped", () => {
+    stack.push();
+    stack.pop();
+    expect(stack.size()).toBe(0);
+  });
   it.todo("throws overflow error when pushing to a stack at full capacity");
   it.todo("throw underflow error when popping an empty stack");
   it.todo("pops the same one item when pushed");
